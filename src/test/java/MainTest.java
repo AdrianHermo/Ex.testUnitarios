656d286b570276261f7990498141ca67734adcb5
Fpineiro <fpineiroandion@danielcastelao.org>
2025-03-19 14:05:11 +0100

    estas son cosas que e tenido que hacer

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,16 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class MainTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
 
     @ParameterizedTest
     @CsvSource({
             "00000000, T, true",
             "12345678, Z, true",
             "87654321, X, true",
-            "00000000, A, false",
-            "12345678, A, false"
+            "11111111, H, true",
+            "99999999, R, true",
+            "12345678, A, false",
+            "00000000, A, false"
     })
     void testComprobarDNI(String dni, char letra, boolean expected) {
         assertEquals(expected, Main.comprobarDNI(dni, letra));
@@ -20,9 +23,23 @@ public class MainTest {
     @CsvSource({
             "00000000, T",
             "12345678, Z",
-            "87654321, X"
+            "87654321, X",
+            "11111111, H",
+            "99999999, R"
     })
     void testCalcularLetraDNI(String dni, char expectedLetter) {
         assertEquals(expectedLetter, Main.calcularLetraDNI(dni));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1234567, false",
+            "123456789, false",
+            "ABCDEFGH, false",
+            "8765432, false",
+            "1, false"
+    })
+    void testInvalidDNILength(String dni, boolean expected) {
+        assertFalse(Main.comprobarDNI(dni, 'T')); // Letra ficticia para probar longitud
     }
 }
